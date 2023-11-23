cd3c605c1dc146c121f31cc335eac00df4eccaf7
CreatorStefann <stefansandor26@gmail.com>
2023-11-23 16:45:32 +0200

    initial function for command type

--- OOPproject/OOPproject/source.cpp
+++ OOPproject/OOPproject/source.cpp
@@ -1,1 +1,18 @@
 #include <iostream>
+#include <string>
+
+int indentifyCommand(const std::string& command)
+{
+	std::string temp;
+	temp = command;
+	temp = strtok(temp, " ");
+	return temp;
+}
+
+int main()
+{
+	std::string command = "SELECT A FROM B";
+	std::string rezultat = indentifyCommand(command);
+	std::cout << rezultat;
+	//return 0;
+}
