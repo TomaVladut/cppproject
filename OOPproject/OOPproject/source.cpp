8c6c3054d48f97426358c81aff5886ac267a9d2e
Toma Vladut <tovlamy26@gmail.com>
2023-11-25 13:54:31 +0200

    merged the previous projects, solved 2 memory leaks in the setter and copy ctr

--- OOPproject/OOPproject/source.cpp
+++ OOPproject/OOPproject/source.cpp
@@ -1,18 +1,149 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 #include <string>
+#include <string.h>
 
-int indentifyCommand(const std::string& command)
-{
-	std::string temp;
-	temp = command;
-	temp = strtok(temp, " ");
-	return temp;
-}
+class CommandProcessor {
+    char* commandString;
+    static const int COMMAND_TYPES_COUNT = 9;
+    const char* commandTypes[COMMAND_TYPES_COUNT] = { "CREATE TABLE", "CREATE INDEX", "DROP TABLE",
+    "DROP INDEX", "DISPLAY TABLE", "INSERT", "SELECT", "UPDATE", "DELETE" };
+
+public:
+    static const int MIN_COMMAND_SIZE = 5;
+
+    CommandProcessor() {
+        this->commandString = new char[1];
+        strcpy_s(commandString, 1, "");
+    }
+
+    CommandProcessor(const char* initialCommand) {
+        if (strlen(initialCommand) >= MIN_COMMAND_SIZE) {
+            this->commandString = new char[strlen(initialCommand) + 1];
+            strcpy_s(this->commandString, strlen(initialCommand) + 1, initialCommand);
+        }
+        else {
+            throw std::invalid_argument("Invalid Command");
+        }
+    }
+
+    CommandProcessor(CommandProcessor& other) {
+        if (this->commandString != nullptr)
+        {
+            delete[] this->commandString;
+        }
+        this->commandString = new char[strlen(other.commandString) + 1];
+        strcpy_s(this->commandString, strlen(other.commandString) + 1, other.commandString);
+    }
+
+    ~CommandProcessor() {
+        delete[] commandString;
+    }
+
+    void SetCommandString(const char* newCommandString)
+    {
+        if (this->commandString != nullptr)
+        {
+            delete[] this->commandString;
+        }
+        if (strlen(newCommandString) >= MIN_COMMAND_SIZE)
+        {
+            this->commandString = new char[strlen(newCommandString) + 1];
+            strcpy_s(this->commandString, strlen(newCommandString) + 1, newCommandString);
+        }
+        else
+            throw new std::exception("Invalid Command");
+    }
+
+    char* GetCommandString()
+    {
+        char* valueOfCommandString = new char[strlen(this->commandString) + 1];
+        strcpy_s(valueOfCommandString, strlen(this->commandString) + 1, this->commandString);
+        return valueOfCommandString;
+    }
+
+    void IdentifyCommandType() {
+        bool commandIdentified = false;
+        for (int i = 0; i < COMMAND_TYPES_COUNT; i++)
+        {
+            if (strstr(commandString, commandTypes[i]) != nullptr)
+            {
+                std::cout << std::endl << "Command type: " << commandTypes[i];
+                commandIdentified = true;
+                break;
+            }
+        }
+        if (!commandIdentified) {
+            std::cout << "Error: Unrecognized command type." << std::endl;
+        }
+    }
+
+    void ValidateCommand() {
+        bool validCommandType = false;
+        for (int i = 0; i < COMMAND_TYPES_COUNT; ++i) {
+            if (strstr(commandString, commandTypes[i]) != nullptr) {
+                validCommandType = true;
+                break;
+            }
+        }
+        if (!validCommandType) {
+            std::cout << "Error: Unrecognized command type." << std::endl;
+            return;
+        }
+        if (strstr(commandString, "CREATE TABLE") != nullptr) {
+            // check if it contains "(column_1_name,type,size, default_value)"
+            // I am thinking of using REGEX for validations because it will get messy...
+            if (strstr(commandString, "(column_1_name,type,size, default_value)") == nullptr) {
+                std::cout << "Error: Invalid format for CREATE TABLE command." << std::endl;
+                return;
+            }
+        }
+        std::cout << "Command is valid." << std::endl;
+    }
+
+    void ExecuteCommand() {
+        std::cout << std::endl << "Executing command: " << commandString;
+        // I will parse all the commands and execute specific tasks after validations and 
+        // using methods, but right now I do not feel like I possess the necassary knowledge
+    }
+
+    void DisplayCommandInfo() {
+        std::cout << std::endl << "Command Info: ";
+        std::cout << std::endl << "Command String: " << commandString;
+        // I will also display the command parameters but for now it looks decent
+    }
+
+};
 
 int main()
 {
-	std::string command = "SELECT A FROM B";
-	std::string rezultat = indentifyCommand(command);
-	std::cout << rezultat;
-	//return 0;
+    try {
+        CommandProcessor cmd1("CREATE TABLE employees (id INT, name TEXT, salary FLOAT);");
+        CommandProcessor cmd2("INSERT INTO employees VALUES (1, 'John Doe', 50000.0);");
+        CommandProcessor cmd3;
+
+        std::cout << "Initial Commands:" << std::endl;
+        std::cout << "cmd1: " << cmd1.GetCommandString() << std::endl;
+        std::cout << "cmd2: " << cmd2.GetCommandString() << std::endl;
+        std::cout << "cmd3: " << (cmd3.GetCommandString() ? cmd3.GetCommandString() : "Empty Command") << std::endl;
+
+        std::cout << "\nModified Commands:" << std::endl;
+
+        cmd3.SetCommandString("SELECT * FROM products;");
+        cmd3.IdentifyCommandType();
+        cmd3.ValidateCommand();
+
+
+        cmd3.DisplayCommandInfo();
+
+
+        cmd3.ExecuteCommand();
+
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
